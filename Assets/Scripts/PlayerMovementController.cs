21621a70637fc7411dae0d89ded886f6fc6e891e
YB912 <YB912TU@gmail.com>
2023-09-22 20:11:04 +0330

    Add transition time between some animation states and simplify player's script

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -2,16 +2,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerMovementController : MonoBehaviour
 {
     public float VerticalSpeedModifier;
     public float HorizontalSpeedModifier;
 
-    private bool _isMoving;
-
     private Vector2 _direction = Vector2.zero;
+
 
     private Rigidbody2D _rigidBody;
     private Animator _animator;
@@ -35,46 +33,28 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Start()
     {
-        _movementAction.started += OnMovement;
-        _movementAction.canceled += OnMovement;
-
         _shootAction.started += OnShoot;
         _shootAction.canceled += OnShoot;
 
         _bombAction.started += OnBomb;
         _bombAction.canceled += OnBomb;
-
-        _movementAction.Enable();
     }
 
     private void Update()
     {
-        if (_isMoving)
-        {
-            _direction = _movementAction.ReadValue<Vector2>().normalized;
-        }
-        _animator.SetInteger("VerticalDirection", Math.Sign(_direction.y));
-        _animator.SetInteger("HorizontalDirection", Math.Sign(_direction.x));
+        _direction = _movementAction.ReadValue<Vector2>().normalized;
     }
     
     private void FixedUpdate()
     {
-            _rigidBody.velocity = new Vector2(_direction.x * HorizontalSpeedModifier * Time.deltaTime,
-                                              _direction.y * VerticalSpeedModifier * Time.deltaTime);
-    }
+        _rigidBody.velocity = new Vector2(_direction.x * HorizontalSpeedModifier * Time.deltaTime,
+                                          _direction.y * VerticalSpeedModifier * Time.deltaTime);
 
-    private void OnMovement(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            _isMoving = true;
-        }
-        else if (context.canceled)
-        {
-            _isMoving = false;
-            _direction = Vector2.zero;
-        }
-        Debug.Log("Movement input detected");
+        // Handle Y axis animation transitions
+        _animator.SetInteger("VerticalDirection", Math.Sign(_rigidBody.velocity.y));
+
+        // Handle X axis animation transitions
+        _animator.SetInteger("HorizontalDirection", Math.Sign(_rigidBody.velocity.x));
     }
 
     private void OnShoot(InputAction.CallbackContext context)
