1fdaa7ab70a451ddcc666de22d0ff1b919f2214b
YB912 <YB912TU@gmail.com>
2023-09-23 20:53:27 +0330

    Scrap mouse movement and try smoothing movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,11 +5,16 @@ using System;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    public float VerticalSpeedModifier;
-    public float HorizontalSpeedModifier;
+    [SerializeField]
+    private float VerticalSpeedModifier;
+    [SerializeField]
+    private float HorizontalSpeedModifier;
+    [SerializeField]
+    private float SmoothInputSpeed;
 
-    private Vector2 _direction = Vector2.zero;
-
+    private Vector2 _newDirection = Vector2.zero;
+    private Vector2 _currentDirection = Vector2.zero;
+    private Vector2 _smoothVelocity;
 
     private Rigidbody2D _rigidBody;
     private Animator _animator;
@@ -42,19 +47,20 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Update()
     {
-        _direction = _movementAction.ReadValue<Vector2>().normalized;
-    }
-    
-    private void FixedUpdate()
-    {
-        _rigidBody.velocity = new Vector2(_direction.x * HorizontalSpeedModifier * Time.deltaTime,
-                                          _direction.y * VerticalSpeedModifier * Time.deltaTime);
+        _newDirection = _movementAction.ReadValue<Vector2>().normalized;
+        _currentDirection = Vector2.SmoothDamp(_currentDirection, _newDirection, ref _smoothVelocity, SmoothInputSpeed);
 
         // Handle Y axis animation transitions
         _animator.SetInteger("VerticalDirection", Math.Sign(_rigidBody.velocity.y));
 
         // Handle X axis animation transitions
         _animator.SetInteger("HorizontalDirection", Math.Sign(_rigidBody.velocity.x));
+    }
+    
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = new Vector2(_currentDirection.x * HorizontalSpeedModifier * Time.deltaTime,
+                                          _currentDirection.y * VerticalSpeedModifier * Time.deltaTime);
     }
 
     private void OnShoot(InputAction.CallbackContext context)
