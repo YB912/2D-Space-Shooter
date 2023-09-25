8eebba244b81f14064b1112d4db2ba0b02558900
YB912 <YB912TU@gmail.com>
2023-09-25 23:00:38 +0330

    Rebuild animator state machines and fix movement
    Fixed movement and its animations as well as firing animations in a new 3x6 state machine format instead of a webbed mess of 18 states. The engine flame has its own object with the ship as its parent and its own animator with 3 states. The ship's animator now has only 6 states to take care of. Animations with 2 identical frames were used to make the illusion of images of the non-firing ship.

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -12,13 +12,17 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField]
     private float SmoothInputSpeed;
 
+    public GameObject DebugSquare;
+
     private Vector2 _newDirection = Vector2.zero;
     private Vector2 _currentDirection = Vector2.zero;
     private Vector2 _smoothVelocity;
 
     private Rigidbody2D _rigidBody;
-    private Animator _animator;
+    private Animator _playerShipAnimator;
+    private Animator _engineFlameAnimator;
     private PlayerInput _playerInput;
+    private SpriteRenderer _spriteRenderer;
 
     private InputAction _movementAction;
     private InputAction _shootAction;
@@ -28,8 +32,10 @@ public class PlayerMovementController : MonoBehaviour
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        _playerShipAnimator = GetComponent<Animator>();
+        _engineFlameAnimator = GameObject.FindGameObjectWithTag("EngineFlame").GetComponent<Animator>();
         _playerInput = GetComponent<PlayerInput>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
 
         _movementAction = _playerInput.actions["Move"];
         _shootAction = _playerInput.actions["Shoot"];
@@ -50,11 +56,16 @@ public class PlayerMovementController : MonoBehaviour
         _newDirection = _movementAction.ReadValue<Vector2>().normalized;
         _currentDirection = Vector2.SmoothDamp(_currentDirection, _newDirection, ref _smoothVelocity, SmoothInputSpeed);
 
-        // Handle Y axis animation transitions
-        _animator.SetInteger("VerticalDirection", Math.Sign(_rigidBody.velocity.y));
+
+        // Handle Y axis and firing animations transitions
+        _playerShipAnimator.SetBool("Firing", _shootAction.ReadValue<float>() == 1 ? true : false);
+        _playerShipAnimator.SetInteger("VerticalDirection", Math.Sign(_newDirection.y));
 
         // Handle X axis animation transitions
-        _animator.SetInteger("HorizontalDirection", Math.Sign(_rigidBody.velocity.x));
+        _engineFlameAnimator.SetInteger("HorizontalDirection", Math.Sign(_newDirection.x));
+
+        // Debug square
+        //DebugSquare.GetComponent<SpriteRenderer>().color = _playerShipAnimator.GetInteger("VerticalDirection") == 1 ? Color.green : Color.red;
     }
     
     private void FixedUpdate()
@@ -65,7 +76,7 @@ public class PlayerMovementController : MonoBehaviour
 
     private void OnShoot(InputAction.CallbackContext context)
     {
-
+        
     }
 
     private void OnBomb(InputAction.CallbackContext context)
